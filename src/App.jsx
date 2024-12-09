21d2383915ba2503df5f8877c368f28ddc2cda0e
GabrielSerafim12 <gserafim794@gmail.com>
2024-12-09 19:33:12 -0300

    enviando projeto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ListaDeTarefas from './components/ListadeTarefas'
+
+function App() {
+
+
+  return (
+    <>
+      <ListaDeTarefas />
+
+
+    </>
+  )
+}
+
+export default App
